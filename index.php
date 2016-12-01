00bca5370a1fdff1b4083e9c48f24932126ed852
premwebdev <premwebdev@gmail.com>
2016-12-01 18:25:08 +0530

    php changed

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+# My first php program
 <?php
-$authorName = "Premnath";
+$authorName = function($name){return $name;};
+function get_year(){
 $year = date("Y");
+return $year;
+}
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +12,7 @@ $year = date("Y");
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Premnath Website</title>
+    <title><?php echo $authorName("Premnath");?> Website</title>
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <link rel="stylesheet" href="./styles/style.css" />
     <link rel="stylesheet" href="./styles/font-awesome.min.css">
@@ -18,7 +22,7 @@ $year = date("Y");
     <header>
       <img src="./Assets/images/PremLogo.png" alt="site logo">
       <div class="logo">
-        <h1><?php echo("$authorName"); ?></h1>
+        <h1><?php echo $authorName("Premnath"); ?></h1>
         <h2>Budding Developer</h2>
       </div>
       <span class="header-right fa fa-angle-down" aria-hidden="true"></span>
@@ -35,7 +39,7 @@ $year = date("Y");
     </section>
 <!--Main footer section -->
     <footer>
-        <p>&copy; <?php echo "$year $authorName"; ?></p>
+        <p class="center-element">&copy; <?php echo get_year()." ".$authorName("Premnath");?></p>
     </footer>
 <!--javascript goes here -->
   </body>
